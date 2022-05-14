696c380299b88af76f494eeb7b568ae70b17febb
Gene Gotimer <eugene.gotimer@steampunk.com>
2022-05-14 12:18:48 -0400

    Finished Lesson 01

--- lesson-01/main.tf
+++ lesson-01/main.tf
@@ -15,10 +15,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_server" {
+  count         = 2
   ami           = "ami-0629230e074c580f2"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "my-server"
+    Name = "tf-lesson-01-${count.index}"
   }
 }
